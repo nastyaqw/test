6cb90586012215cf12b629544669209e49f6a0a4
Бирюкова Анастасия <biryukovaam@301c4.occ.local>
2022-10-15 09:28:36 +0400

    models

---
+++ laravel/database/migrations/2022_10_15_051332_create_galleries_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->incriments('id');
+            $table->photo();
+            $table->title();
+            $table->describtion();
+            $table->userId();
+            $table->categoryId();
+            $table->Active();
+            $table->isPublished();
+            $table->publishAt();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galleries');
+    }
+}
